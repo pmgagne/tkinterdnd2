64a151f6808146b5bfb3ec12eb3ea0fee8679c25
Philippe Gagné <philippemgagne@icloud.com>
2020-02-17 20:43:53 -0500

    Add tk extension to tkinter path.

--- TkinterDnD2/TkinterDnD.py
+++ TkinterDnD2/TkinterDnD.py
@@ -36,6 +36,20 @@ def _require(tkroot):
     '''Internal function.'''
     global TkdndVersion
     try:
+        import os.path
+        import platform
+
+        if platform.system()=="Darwin":
+            tkdnd_platform_rep = "osx64"
+        elif platform.system()=="Linux":
+            tkdnd_platform_rep = "linux64"
+        elif platform.system()=="Windows":
+            tkdnd_platform_rep = "win64"
+        else:
+            raise RuntimeError('Plaform not supported.')
+        
+        module_path = os.path.join(os.path.dirname(__file__), 'tkdnd', tkdnd_platform_rep)
+        TkdndVersion = tkroot.tk.call('lappend', 'auto_path', module_path)
         TkdndVersion = tkroot.tk.call('package', 'require', 'tkdnd')
     except tkinter.TclError:
         raise RuntimeError('Unable to load tkdnd library.')
