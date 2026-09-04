fc1e0936fabeefbe0addf745833b57a29a2200cd
Squiblydoo <77356206+Squiblydoo@users.noreply.github.com>
2026-09-04 09:14:25 -0400

    Update binaries to tkdnd2.10.2

--- tkinterdnd2/tkdnd/linux-arm64/pkgIndex.tcl
+++ tkinterdnd2/tkdnd/linux-arm64/pkgIndex.tcl
@@ -53,10 +53,10 @@ namespace eval ::tkdnd {
 
 }; # namespace ::tkdnd
 
-package ifneeded tkdnd 2.10.1 \
+package ifneeded tkdnd 2.10.2 \
   "tkdnd::source \{$dir/tkdnd.tcl\} ; \
-   tkdnd::initialise \{$dir\} libtkdnd2.10.1.so tkdnd"
+   tkdnd::initialise \{$dir\} libtkdnd2.10.2.so tkdnd"
 
-package ifneeded tkdnd::utils 2.10.1 \
+package ifneeded tkdnd::utils 2.10.2 \
   "tkdnd::source \{$dir/tkdnd_utils.tcl\} ; \
-   package provide tkdnd::utils 2.10.1"
+   package provide tkdnd::utils 2.10.2"
--- tkinterdnd2/tkdnd/win-x86-tcl9/pkgIndex.tcl
+++ tkinterdnd2/tkdnd/win-x86-tcl9/pkgIndex.tcl
@@ -53,10 +53,10 @@ namespace eval ::tkdnd {
 
 }; # namespace ::tkdnd
 
-package ifneeded tkdnd 2.10.1 \
+package ifneeded tkdnd 2.10.2 \
   "tkdnd::source \{$dir/tkdnd.tcl\} ; \
-   tkdnd::initialise \{$dir\} libtkdnd2.10.1[info sharedlibextension] tkdnd"
+   tkdnd::initialise \{$dir\} libtkdnd2.10.2[info sharedlibextension] tkdnd"
 
-package ifneeded tkdnd::utils 2.10.1 \
+package ifneeded tkdnd::utils 2.10.2 \
   "tkdnd::source \{$dir/tkdnd_utils.tcl\} ; \
-   package provide tkdnd::utils 2.10.1"
+   package provide tkdnd::utils 2.10.2"
